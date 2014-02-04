d5ca12fb45e0b1623488a2a8db8eab6403974783
Deepti Buchi <dbuchi@zendesk.com>
2014-02-04 14:00:53 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bluebells::Application.config.secret_key_base = '605de5ab9aa862c809a52a85a5a656e35a73121974b2557b5cc33c0fb26abd090891ffaaa1b895938a0bac0840ab71ad27ba6b7ed14424fa953d03372e5963f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bluebells::Application.config.session_store :cookie_store, key: '_bluebells_session'
